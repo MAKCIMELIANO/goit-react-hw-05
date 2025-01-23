f9520435063db4712b89971792886b7beea5738f
MAKCIMELIANO <makcimeliano@gmail.com>
2025-01-24 00:01:34 +0200

    components and page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,35 @@
+// import { Suspense, lazy } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import Navigation from '../Navigation/Navigation';
+
+// const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+// const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'));
+// const MovieDetailsPage = lazy(() =>
+//   import('../../pages/MovieDetailsPage/MovieDetailsPage')
+// );
+// const NotFoundPage = lazy(() =>
+//   import('../../pages/NotFoundPage/NotFoundPage')
+// );
+
+// function App() {
+//   return (
+//     <>
+//       <Navigation />
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <Routes>
+//           <Route path="/" element={HomePage} />
+//           <Route path="/movies" element={MoviesPage} />
+//           <Route path="/movies/:movieId" element={MovieDetailsPage} />
+//           <Route path="*" element={NotFoundPage} />
+//         </Routes>
+//       </Suspense>
+//     </>
+//   );
+// }
+
+// export default App;
 import css from './App.module.css';
+
 const App = () => {
   return <div className={css.container}>App</div>;
 };
