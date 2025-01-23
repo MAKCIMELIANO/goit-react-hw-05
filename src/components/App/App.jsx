a81c83eb6adbfdf09c9461e35bf19fb328c9f7a0
MAKCIMELIANO <makcimeliano@gmail.com>
2025-01-23 21:46:52 +0200

    commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,18 @@
 import css from './App.module.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from '../Layout/Layout';
 import Sales from '../../pages/Sales/Sales';
 import Invoices from '../Invoices/Invoices';
 import InvoiceDetails from '../InvoiceDetails/InvoiceDetails';
+import Customers from '../../pages/Customers/Customers';
+import CustomersDetails from '../../pages/CustomersDetails/CustomersDetails';
 
 const App = () => {
   return (
     <div className={css.container}>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="dashboard" />} />
           <Route path="dashboard" element={<div>Dashboard</div>} />
           <Route path="sales" element={<Sales />}>
             <Route index element={<div>Overview hello</div>} />
@@ -22,7 +25,8 @@ const App = () => {
           </Route>
           <Route path="reports" element={<div>Reports</div>} />
           <Route path="feedback" element={<div>Feedback</div>} />
-          <Route path="customers" element={<div>Customers</div>} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="customers/:customerId" element={<CustomersDetails />} />
         </Route>
       </Routes>
     </div>
