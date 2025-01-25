e79a9410cbe38cb033f6c42789b7410fba40e1f3
MAKCIMELIANO <makcimeliano@gmail.com>
2025-01-25 13:15:26 +0200

    basic logic

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,37 +1,23 @@
-// import { Suspense, lazy } from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import Navigation from '../Navigation/Navigation';
-
-// const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
-// const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'));
-// const MovieDetailsPage = lazy(() =>
-//   import('../../pages/MovieDetailsPage/MovieDetailsPage')
-// );
-// const NotFoundPage = lazy(() =>
-//   import('../../pages/NotFoundPage/NotFoundPage')
-// );
-
-// function App() {
-//   return (
-//     <>
-//       <Navigation />
-//       <Suspense fallback={<div>Loading...</div>}>
-//         <Routes>
-//           <Route path="/" element={HomePage} />
-//           <Route path="/movies" element={MoviesPage} />
-//           <Route path="/movies/:movieId" element={MovieDetailsPage} />
-//           <Route path="*" element={NotFoundPage} />
-//         </Routes>
-//       </Suspense>
-//     </>
-//   );
-// }
-
-// export default App;
+import { Route, Routes } from 'react-router-dom';
 import css from './App.module.css';
+import Navigation from '../Navigation/Navigation';
+import HomePage from '../../pages/HomePage/HomePage';
+import MoviesPage from '../../pages/MoviesPage/MoviesPage';
+import MovieDetailsPage from '../../pages/MovieDetailsPage/MovieDetailsPage';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 
 const App = () => {
-  return <div className={css.container}>App</div>;
+  return (
+    <div className={css.container}>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
